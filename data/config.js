5daa7061d4e989095bcab4f7076aea21065e3c63
madhu.rondla <madhu.rondla@splashbi.com>
2023-04-14 11:01:39 +0530

    updtaed exec type

--- data/config.js
+++ data/config.js
@@ -3,6 +3,6 @@ module.exports={
     "password": "welcome",
     "browser":"chrome",
     //for firefox use 'geckodriver', for chrome use 'chromedriver' and for grid execution use 'docker'
-    "executionService":"docker",
+    "executionService":"chromedriver",
     "maxInstances": 2
 }
